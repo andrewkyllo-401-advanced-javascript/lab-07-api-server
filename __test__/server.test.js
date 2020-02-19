fb1e567ccdcd26c05da06eed22a4d155a9969e2a
Andrew Kyllo <kyllo34@gmail.com>
2020-02-18 17:05:01 -0800

    completed all routes and verified with unit tests

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -3,9 +3,29 @@ const supertest = require('supertest');
 const mockRequest = supertest(server);
 
 describe('Our server.', () => {
+  it('Responds correctly on create request to /categories.', () => {
+    return mockRequest
+      .post('/categories')
+      .send({"id": 4,"name": "cars"})
+      .then(results => {
+        expect(results.status).toBe(201);
+        expect(results.body).toStrictEqual({"id": 4,"name": "cars"})
+      })
+      .catch(console.error);
+  });
+  it('Responds correctly on create request to /products.', () => {
+    return mockRequest
+    .post('/products')
+    .send({"id": 4,"name": "cars", "description": "car-like"})
+    .then(results => {
+      expect(results.status).toBe(201);
+      expect(results.body).toStrictEqual({"id": 4,"name": "cars", "description": "car-like"})
+    })
+    .catch(console.error);
+  });
   it('Responds correctly on request to /categories.', () => {
     return mockRequest
-      .get('/categories')
+    .get('/categories')
       .then(results => {
         expect(results.status).toBe(200);
       })
@@ -19,12 +39,27 @@ describe('Our server.', () => {
       })
       .catch(console.error);
   });
-  it('Should modify selected object in /categories', () => {
+  it('Should modify selected object in /products.', () => {
+    return mockRequest
+      .put('/products/1')
+      .send({name:"hot dog", description: "hot doggish"})
+      .then(results => {
+        expect(results.status).toBe(202);
+        expect(results.body.name).toBe('hot dog');
+        expect(results.body.description).toBe('hot doggish');
+      })
+      .catch(console.error);
+  })
+  it('Should modify selected object in /categories.', () => {
     return mockRequest
       .put('/categories/1')
-      .send()
+      .send({name:"cars"})
+      .then(results => {
+        expect(results.status).toBe(202);
+        expect(results.body.name).toBe('cars');
+      })
+      .catch(console.error);
   })
-
 
   it('Responds correctly on delete request to /categories.', () => {
     return mockRequest
